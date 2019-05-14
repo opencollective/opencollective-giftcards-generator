e6a261fbd7005f80d87a19f2f0643698b838cc91
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-05-14 22:37:34 +0200

    chore(PrintableGiftCard): Add snapshot testing

---
+++ test/components/PrintableGiftCard.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { IntlProvider } from 'react-intl';
+
+import PrintableGiftCard from '../../src/components/PrintableGiftCard';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <IntlProvider>
+        <PrintableGiftCard
+          amount={50000}
+          currency="USD"
+          code="8X4WWD2G"
+          expiryDate="Wed May 13 2020 00:00:00 GMT+0200 (GMT+02:00)"
+          withQRCode={false}
+          tagline="I have no QR code!"
+          style={{ marginRight: 15 }}
+        />
+      </IntlProvider>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
